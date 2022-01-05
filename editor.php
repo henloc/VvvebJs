f821434ebd3403f7f6e116d72f1b83441fac391e
Henning Loch <henning.loch@stud.hn.de>
2022-01-05 23:04:05 +0100

    change directory to pages and add login

--- editor.php
+++ editor.php
@@ -1,23 +1,34 @@
 <?php
-//include 'editor.html';
-$html = file_get_contents('editor.html');
+// Initialize the session
 
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../../login.php");
+    exit;
+}
+
+//include 'editor.html.php';
+$html = file_get_contents('editor.html.php');
+$files = "";
 //search for html files in demo and my-pages folders
-$htmlFiles = glob('{my-pages/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
-foreach ($htmlFiles as $file) { 
-   if (in_array($file, array('new-page-blank-template.html', 'editor.html'))) continue;//skip template files
-   $pathInfo = pathinfo($file);
-   $filename = $pathInfo['filename'];
-   $folder = preg_replace('@/.+?$@', '', $pathInfo['dirname']);
-   $subfolder = preg_replace('@^.+?/@', '', $pathInfo['dirname']);
-   if ($filename == 'index' && $subfolder) {
-	   $filename = $subfolder;
-   }
-   $url = $pathInfo['dirname'] . '/' . $pathInfo['basename'];
-   $name = ucfirst($filename);
+$htmlFiles = glob('{pages/*.html,demo/*\/*.html, demo/*.html}',  GLOB_BRACE);
+foreach ($htmlFiles as $file) {
+    if (in_array($file, array('new-page-blank-template.html', 'editor.html.php'))) continue;//skip template files
+    $pathInfo = pathinfo($file);
+    $filename = $pathInfo['filename'];
+    $folder = preg_replace('@/.+?$@', '', $pathInfo['dirname']);
+    $subfolder = preg_replace('@^.+?/@', '', $pathInfo['dirname']);
+    if ($filename == 'index' && $subfolder) {
+        $filename = $subfolder;
+    }
+    $url = $pathInfo['dirname'] . '/' . $pathInfo['basename'];
+    $name = ucfirst($filename);
 
-  $files .= "{name:'$name', file:'$filename', title:'$name',  url: '$url', folder:'$folder'},";
-} 
+    $files .= "{name:'$name', file:'$filename', title:'$name',  url: '$url', folder:'$folder'},";
+}
 
 
 //replace files list from html with the dynamic list from demo folder
